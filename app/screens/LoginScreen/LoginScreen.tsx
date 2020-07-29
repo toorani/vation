4c7e43fa02cd7592fbcee0ba379bbb74e50e0493
Toorani <toorani@Toorani-MacBook-Pro.local>
2020-07-29 20:59:32 +0200

    login screen clean code

--- app/screens/LoginScreen/LoginScreen.tsx
+++ app/screens/LoginScreen/LoginScreen.tsx
@@ -7,13 +7,13 @@ import { LinearGradient } from 'expo-linear-gradient'
 import StyleConfigs from '../../Shared/StyleConfigs';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import SampleData from '../../DataModels/SampleData';
+import CircleBar from '../../Shared/CircleBar';
+import LoginForm from './Components/LoginForm';
 
 const LoginScreen = ({ navigation }) => {
 
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
 
-    async function onLogin() {
+    async function onLogin(email: string, password: string) {
         try {
             await loginWithEmail(email.trim(), password);
             navigation.navigate('Home', { data: new SampleData() });
@@ -22,9 +22,6 @@ const LoginScreen = ({ navigation }) => {
             Alert.alert(error.message)
         }
     }
-
-   
-
 
     return (
         <View style={styles.container}>
@@ -37,57 +34,23 @@ const LoginScreen = ({ navigation }) => {
                     </TouchableOpacity>
                 </View>
                 <View style={styles.circlesContainer}>
-                    <View style={styles.circles}>
-                        <View style={styles.circle}></View>
-                        <View style={styles.circleGr}></View>
-                        <View style={styles.circleGr}></View>
-
-                    </View>
+                    <CircleBar style={styles.circles} count={3} selectedIndex={0} />
                 </View>
             </LinearGradient>
-
-            <View style={styles.formSection}>
-                <View style={styles.welcomeContainer}>
+            <View style={styles.formContainer}>
+                <View style={styles.titleContainer}>
                     <Text style={styles.welcomeBack}>Welcome back</Text>
                     <Text style={styles.loginTitle}>Login to your account</Text>
-                    <View style={styles.loginForm}>
-                        <View style={styles.textBoxContainer}>
-                            <TextInput
-                                style={styles.textBox}
-                                value={email}
-                                placeholder='Email'
-                                onChangeText={(text) => { setEmail(text) }} />
-                            <TextInput
-                                style={styles.textBox}
-                                value={password}
-                                placeholder='Password'
-                                secureTextEntry
-                                returnKeyType='go'
-                                autoCorrect={false}
-                                onChangeText={(text) => { setPassword(text) }} />
-                            <TextInput />
-                        </View>
-                        <View style={{ width: '80%' }}>
-                            <Button
-                                buttonStyle={styles.loginButton}
-                                titleStyle={styles.loginButtonText}
-                                title="Login"
-                                onPress={async () => onLogin()}
-                            />
-                        </View>
-                        <View style={styles.forgotContainer}>
-                            <Text style={styles.forgotPassword}>Forgot your password?</Text>
-                        </View>
-                        <View style={styles.signUpContainer}>
-                            <Text style={styles.dontHaveAccount}>Don't have an account?</Text>
-                            <Text style={styles.signUp}>Sign up</Text>
-                        </View>
-
-
+                </View>
+                <LoginForm onLogin={async (email,password) => onLogin(email,password)} style={{ top: 70 }} />
+                <View style={styles.moreActionContainer}>
+                    <Text style={styles.forgotPassword}>Forgot your password?</Text>
+                    <View style={styles.signUpContainer}>
+                        <Text style={styles.dontHaveAccount}>Don't have an account?</Text>
+                        <Text style={styles.signUp}>Sign up</Text>
                     </View>
                 </View>
             </View>
-
         </View>
 
     );
@@ -106,37 +69,18 @@ const styles = StyleSheet.create({
     },
     circlesContainer: {
         alignItems: 'center',
-    
         top: Platform.OS === 'ios' ? '65%' : '25%',
+    },
+    circles: {
+        top: Platform.OS === 'ios' ? '15%' : '8%',
     },
     backButtonContainer: {
         top: Platform.OS === 'ios' ? '15%' : '35%',
         left: '8%',
-        width:50,
+        width: 50,
     },
-    circles: {
-        top: Platform.OS === 'ios' ? '15%' : '8%',
 
-        flexDirection: 'row',
-        flex: 1,
-        alignItems: 'center'
-    },
-    circle: {
-        width: 10,
-        height: 10,
-        borderRadius: 5,
-        margin: 5,
-        backgroundColor: '#fff'
-    },
-    circleGr: {
-        width: 10,
-        height: 10,
-        borderRadius: 5,
-        margin: 5,
-        backgroundColor: '#fff',
-        opacity: 0.5,
-    },
-    formSection: {
+    formContainer: {
         flexDirection: 'column',
         shadowColor: "#000",
         shadowOffset: {
@@ -146,14 +90,17 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.5,
         shadowRadius: 30,
         backgroundColor: '#fff',
-        height: Platform.OS === 'ios' ? '55%' : '80%',
+        height: '100%',
         top: -20,
         borderTopLeftRadius: 25,
         borderTopRightRadius: 25,
     },
-
+    titleContainer: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        top: 40,
+    },
     welcomeBack: {
-
         fontFamily: StyleConfigs.fonts.Avenir,
         fontStyle: "normal",
         fontSize: 34,
@@ -161,13 +108,7 @@ const styles = StyleSheet.create({
         lineHeight: 46,
         color: '#0A1F44'
     },
-    welcomeContainer: {
-        top: 40,
-        flex: 1,
-        flexDirection: 'column',
 
-        alignItems: 'center'
-    },
     loginTitle: {
         fontFamily: StyleConfigs.fonts.Roboto,
         fontStyle: "normal",
@@ -177,47 +118,12 @@ const styles = StyleSheet.create({
         color: '#000',
         opacity: 0.4,
     },
-    loginForm: {
-        flex: 1,
+
+    moreActionContainer: {
         flexDirection: 'column',
         width: '100%',
         alignItems: 'center',
-        top: 20,
-
-    },
-    textBox: {
-        backgroundColor: '#f1f1f2',
-        borderRadius: 25,
-        height: 44,
-        width: '80%',
-        margin: 5,
-        fontFamily: StyleConfigs.fonts.Roboto,
-        fontStyle: "normal",
-        fontSize: 17,
-        fontWeight: 'normal',
-        lineHeight: 20,
-        paddingLeft: 20,
-    },
-    textBoxContainer: {
-        alignItems: 'center',
-        width: '100%',
-        margin: 10,
-    },
-
-    loginButton: {
-        backgroundColor: '#6C9DFE',
-        borderRadius: 50,
-        alignItems: "center"
-    },
-    loginButtonText: {
-        fontFamily: StyleConfigs.fonts.Avenir,
-        fontStyle: 'normal',
-        fontWeight: '800',
-        fontSize: 17,
-        lineHeight: 24,
-    },
-    forgotContainer: {
-        marginTop: 35,
+        top: 120,
     },
     forgotPassword: {
         fontFamily: StyleConfigs.fonts.Roboto,
@@ -229,7 +135,7 @@ const styles = StyleSheet.create({
     },
     signUpContainer: {
         flexDirection: "row",
-        marginTop: 35,
+        marginTop: 25,
     },
     dontHaveAccount: {
         fontFamily: StyleConfigs.fonts.Roboto,
